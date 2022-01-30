6cd757914b457851dfe4415fe34d1c29daf8e835
Rob Gannon <cyberpeaches@gmail.com>
2022-01-30 23:04:42 +0000

    Add animations and links over landing image

--- assets/javascript/language.js
+++ assets/javascript/language.js
@@ -5,19 +5,25 @@ let home = document.getElementById("home");
 let about = document.getElementById("about");
 let videos = document.getElementById("videos");
 let contact = document.getElementById("contact");
+let welcome_msg_1 = document.getElementById("welcome_msg_1");
+let welcome_msg_2 = document.getElementById("welcome_msg_2");
 
 const siteTextEnglish = {
     home: 'Home',
     about: 'About',
     videos: 'Videos',
-    contact: 'Contact'
+    contact: 'Contact',
+    welcome_msg_1: 'Welcome to An Tobair Segais!',
+    welcome_msg_2: 'Click here to learn more',
 }
 
 const siteTextPortuguese = {
     home: 'Início',
     about: 'Sobre',
     videos: 'Vídeos',
-    contact: 'Contato'
+    contact: 'Contato',
+    welcome_msg_1: 'Bem-vindo a An Tobair Segais!',
+    welcome_msg_2: 'Clique aqui para saber mais',
 }
 
 const setEnglish = () => {
@@ -26,6 +32,8 @@ const setEnglish = () => {
     about.innerHTML = siteTextEnglish.about;
     videos.innerHTML = siteTextEnglish.videos;
     contact.innerHTML = siteTextEnglish.contact;
+    welcome_msg_1.innerHTML = siteTextEnglish.welcome_msg_1;
+    welcome_msg_2.innerHTML = siteTextEnglish.welcome_msg_2;
 }
 
 const setPortugese = () => {
@@ -34,4 +42,6 @@ const setPortugese = () => {
     about.innerHTML = siteTextPortuguese.about;
     videos.innerHTML = siteTextPortuguese.videos;
     contact.innerHTML = siteTextPortuguese.contact;
+    welcome_msg_1.innerHTML = siteTextPortuguese.welcome_msg_1;
+    welcome_msg_2.innerHTML = siteTextPortuguese.welcome_msg_2;
 }
