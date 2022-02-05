1b4324741224416bb72dfa43528d7ec087596cb6
Rob Gannon <cyberpeaches@gmail.com>
2022-02-05 15:23:25 +0000

    Fix language button to only one button and change flag on click

--- assets/javascript/language.js
+++ assets/javascript/language.js
@@ -7,6 +7,7 @@ let videos = document.getElementById("videos");
 let contact = document.getElementById("contact");
 let welcome_msg_1 = document.getElementById("welcome_msg_1");
 let welcome_msg_2 = document.getElementById("welcome_msg_2");
+let language_button = document.getElementById("language_button");
 
 const siteTextEnglish = {
     home: 'Home',
@@ -26,22 +27,24 @@ const siteTextPortuguese = {
     welcome_msg_2: 'Clique aqui para saber mais',
 }
 
-const setEnglish = () => {
-    html.setAttribute('lang', 'en');
-    home.innerHTML = siteTextEnglish.home;
-    about.innerHTML = siteTextEnglish.about;
-    videos.innerHTML = siteTextEnglish.videos;
-    contact.innerHTML = siteTextEnglish.contact;
-    welcome_msg_1.innerHTML = siteTextEnglish.welcome_msg_1;
-    welcome_msg_2.innerHTML = siteTextEnglish.welcome_msg_2;
-}
-
-const setPortugese = () => {
-    html.setAttribute('lang', 'pt');
-    home.innerHTML = siteTextPortuguese.home;
-    about.innerHTML = siteTextPortuguese.about;
-    videos.innerHTML = siteTextPortuguese.videos;
-    contact.innerHTML = siteTextPortuguese.contact;
-    welcome_msg_1.innerHTML = siteTextPortuguese.welcome_msg_1;
-    welcome_msg_2.innerHTML = siteTextPortuguese.welcome_msg_2;
+const setLanguage = () => {
+    if (html.getAttribute('lang') == "en"){
+        html.setAttribute('lang', 'pt');
+        language_button.style.cssText += 'background-image: url(assets/images/unionjackflag20x40.png);';
+        home.innerHTML = siteTextPortuguese.home;
+        about.innerHTML = siteTextPortuguese.about;
+        videos.innerHTML = siteTextPortuguese.videos;
+        contact.innerHTML = siteTextPortuguese.contact;
+        welcome_msg_1.innerHTML = siteTextPortuguese.welcome_msg_1;
+        welcome_msg_2.innerHTML = siteTextPortuguese.welcome_msg_2;
+    } else {
+        html.setAttribute('lang', 'en');
+        language_button.style.cssText += "background-image: url(assets/images/braziltumbnail20x40.png);";
+        home.innerHTML = siteTextEnglish.home;
+        about.innerHTML = siteTextEnglish.about;
+        videos.innerHTML = siteTextEnglish.videos;
+        contact.innerHTML = siteTextEnglish.contact;
+        welcome_msg_1.innerHTML = siteTextEnglish.welcome_msg_1;
+        welcome_msg_2.innerHTML = siteTextEnglish.welcome_msg_2;
+    }
 }
