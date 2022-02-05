03ed9becce18a37615c34513e2351f90835188ed
Rob Gannon <cyberpeaches@gmail.com>
2022-02-05 22:20:51 +0000

    Fix: responsiveness on arrow and welcome message. Remove videos link from nav

--- assets/javascript/language.js
+++ assets/javascript/language.js
@@ -3,7 +3,6 @@ let h1_left = document.getElementById("h1_left");
 let h1_right = document.getElementById("h1_right");
 let home = document.getElementById("home");
 let about = document.getElementById("about");
-let videos = document.getElementById("videos");
 let contact = document.getElementById("contact");
 let welcome_msg_1 = document.getElementById("welcome_msg_1");
 let welcome_msg_2 = document.getElementById("welcome_msg_2");
@@ -12,7 +11,6 @@ let language_button = document.getElementById("language_button");
 const siteTextEnglish = {
     home: 'Home',
     about: 'About',
-    videos: 'Videos',
     contact: 'Contact',
     welcome_msg_1: 'Welcome to An Tobair Segais!',
     welcome_msg_2: 'Click here to learn more',
@@ -21,7 +19,6 @@ const siteTextEnglish = {
 const siteTextPortuguese = {
     home: 'Início',
     about: 'Sobre',
-    videos: 'Vídeos',
     contact: 'Contato',
     welcome_msg_1: 'Bem-vindo a An Tobair Segais!',
     welcome_msg_2: 'Clique aqui para saber mais',
@@ -33,7 +30,6 @@ const setLanguage = () => {
         language_button.style.cssText += 'background-image: url(assets/images/unionjackflag20x40.png);';
         home.innerHTML = siteTextPortuguese.home;
         about.innerHTML = siteTextPortuguese.about;
-        videos.innerHTML = siteTextPortuguese.videos;
         contact.innerHTML = siteTextPortuguese.contact;
         welcome_msg_1.innerHTML = siteTextPortuguese.welcome_msg_1;
         welcome_msg_2.innerHTML = siteTextPortuguese.welcome_msg_2;
@@ -42,7 +38,6 @@ const setLanguage = () => {
         language_button.style.cssText += "background-image: url(assets/images/braziltumbnail20x40.png);";
         home.innerHTML = siteTextEnglish.home;
         about.innerHTML = siteTextEnglish.about;
-        videos.innerHTML = siteTextEnglish.videos;
         contact.innerHTML = siteTextEnglish.contact;
         welcome_msg_1.innerHTML = siteTextEnglish.welcome_msg_1;
         welcome_msg_2.innerHTML = siteTextEnglish.welcome_msg_2;
