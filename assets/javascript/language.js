c70b87e36daffa5006a41a3d8402fe8123f3bc48
Rob Gannon <cyberpeaches@gmail.com>
2022-02-12 20:56:33 +0000

    Feat: update language.js with further aria-label, alt, placehonder, and text in portuguese

--- assets/javascript/language.js
+++ assets/javascript/language.js
@@ -16,7 +16,8 @@ let testimonial_3 = document.getElementById("testimonial_3");
 let testimonial_4 = document.getElementById("testimonial_4");
 let contact_form_legend = document.getElementById("contact_form_legend");
 let title = document.getElementById("title");
-
+let message_label = document.querySelectorAll('[for="message_field"]'); 
+let submit_button_text = document.querySelectorAll('[class="button_submit"]');
 /* ARIA LABELS */
 let msg_container = document.getElementById("msg_container");
 let landing_image_link_aria = document.getElementsByClassName("welcome_msg")
@@ -30,9 +31,14 @@ let language_button_aria = document.querySelectorAll('[id="language_button"]');
 let language_button_tooltip_aria = document.querySelectorAll('[class="tooltip langtip"]');
 /* IMAGE ALTS */
 let chevron_alt = document.getElementById('chevron_icon'); 
-let accessibility_icon_alt = document.querySelectorAll('[alt="accessibility icon"]');
-/* BUTTON */
-/* let language_button = document.getElementById("language_button"); */
+let accessibility_icon_alt = document.querySelectorAll('[alt="accessibility icon"]'); 
+let therapy_woman_alt = document.querySelectorAll('[alt="Woman getting therapy online"]');
+let half_banner_alt = document.querySelectorAll('[srcset="assets/images/bannerleft.png"]');
+let full_banner_alt = document.querySelectorAll('[src="assets/images/fullbanner.png"]');
+
+/* PLACEHOLDERS */ 
+let email_placeholder = document.querySelectorAll('[id="email_field"]'); 
+let message_placeholder = document.querySelectorAll('[id="message_field"]');
 
 /* ENGLISH SITE VERSION */
 const siteTextEnglish = {
@@ -44,10 +50,10 @@ const siteTextEnglish = {
     welcome_msg_3: 'helping minds to grow',
     h1_right: 'AN TOBAIR SEGAIS - INTERPRETING MENTORS',
     testimonials_section_heading: 'TESTIMONIALS',
-    testimonial_1: 'Most popular self help<br>content creaters online<br>interpreted',
-    testimonial_2: 'Most popular self help<br>content creaters online<br>interpreted',
-    testimonial_3: 'Most popular self help<br>content creaters online<br>interpreted',
-    testimonial_4: 'Most popular self help<br>content creaters online<br>interpreted',
+    testimonial_1: 'The audio quality is so professional and clear.',
+    testimonial_2: 'Thank you for your effort in translating this content.',
+    testimonial_3: "It's great to have access to this life changing content",
+    testimonial_4: 'Her voice is very pleasant to listen to',
     about_section_heading: 'ABOUT',
     about_ats: 'An Tobar Segais Interpreting Mentors project started from the desire and ambition to bring educational mental health content to Portuguese speakers and share with English speakers a compilation of the most qualified mentors out there.<br>The main areas covered here are related to self-improvement based on psychology, neuroscience, stoicism, western and eastern philosophy.<br>Thank you for checking in. And, Keep improving',
     contact_form_legend: 'Contact',
@@ -60,23 +66,20 @@ const siteTextPortuguese = {
     home: 'Início',
     about: 'Sobre',
     contact: 'Contato',
-    welcome_msg_1: 'Bem-vindo a An Tobair Segais!',
+    welcome_msg_1: 'Bem-vindo ao An Tobair Segais!',
     welcome_msg_2: 'Conectando pessoas e..',
-    welcome_msg_3: 'ajudando mentes a crescer',
+    welcome_msg_3: 'ajudando mentes à crescer',
     h1_right: 'AN TOBAIR SEGAIS - INTERPRETANDO MENTORES',
     testimonials_section_heading: 'TESTEMUNHOS',
-    testimonial_1: 'Os criadores de conteúdo<br>de auto-ajuda mais<br>populares online<br>interpretados',
-    testimonial_2: 'Os criadores de conteúdo<br>de auto-ajuda mais<br>populares online<br>interpretados',
-    testimonial_3: 'Os criadores de conteúdo<br>de auto-ajuda mais<br>populares online<br>interpretados',
-    testimonial_4: 'Os criadores de conteúdo<br>de auto-ajuda mais<br>populares online<br>interpretados',
+    testimonial_1: 'A qualidade do áudio é tão professional e clara.',
+    testimonial_2: 'Obrigada pelo seu esforço em traduzir este conteúdo.',
+    testimonial_3: 'É ótimo ter acesso à este tipo de conteúdo tão relevante.',
+    testimonial_4: 'A voz dela é muito agradável de escutar.',
     about_section_heading: 'SOBRE',
     about_ats: 'An Tobar Segais - Interpretando Mentores, nasceu do desejo e ambição de levar conteúdo educacional sobre saúde mental para falantes da língua portuguesa e compartilhar com falantes da língua inglesa uma compilação dos mais qualificados mentores disponíveis no Youtube.<br>As principais áreas abordadas aqui são relacionadas com aperfeiçoamento pessoal e auto-conhecimento, com base na psicologia, neurociência, estoicismo, filosofia ocidental e oriental.<br>Obrigada por aparecer por aqui. Continue se aperfeiçoando',
     contact_form_legend: 'Contato',
     title: 'An Tobair Segais - Interpretando Mentores',
-    
-
 }
-
 
 /* TOGGLE SITE LANGUAGE */
 const setLanguage = () => {
@@ -111,6 +114,13 @@ const setLanguage = () => {
         language_button_aria[0].setAttribute('aria-label', 'Botão pra mudar o idioma entre português e inglês'); 
         language_button_tooltip_aria[0].setAttribute('aria-label', 'Idioma');
         language_button_tooltip_aria[0].innerHTML = 'Idioma';
+        therapy_woman_alt[0].setAttribute('alt', 'Mulher fazendo terapia online'); 
+        half_banner_alt[0].setAttribute('alt', 'Imagem em linha reta de quatro balões de diálogo com uma cabeça de mulher dentro de cada um deles');
+        full_banner_alt[0].setAttribute('alt', 'Imagem em linha reta de sete balões de diálogo com uma cabeça de mulher dentro de cada um deles'); 
+        email_placeholder[0].setAttribute('placeholder', 'exemplo@seuemail.com');
+        message_label[0].innerHTML = 'Mensagem'; 
+        message_placeholder[0].setAttribute('placeholder', 'Por favor digite sua mensagem aqui'); 
+        submit_button_text[0].setAttribute('value', 'Enviar');
     } else {
         html.setAttribute('lang', 'en');
         language_button.style.cssText += "background-image: url(assets/images/braziltumbnail20x40.png);";
@@ -142,5 +152,12 @@ const setLanguage = () => {
         language_button_aria[0].setAttribute('aria-label', 'Button to switch language between Portuguese and English');
         language_button_tooltip_aria[0].setAttribute('aria-label', 'Language');
         language_button_tooltip_aria[0].innerHTML = 'Language';
+        therapy_woman_alt[0].setAttribute('alt', 'Woman getting therapy online');
+        half_banner_alt[0].setAttribute('alt', 'Half of a banner image of heads with speech bubbles.');
+        full_banner_alt[0].setAttribute('alt', 'Banner image of heads with speech bubbles');
+        email_placeholder[0].setAttribute('placeholder', 'example@yourmail.com');
+        message_label[0].innerHTML = 'Message';
+        message_placeholder[0].setAttribute('placeholder', 'Please type your message here');
+        submit_button_text[0].setAttribute('value', 'Submit');
     }
 }
